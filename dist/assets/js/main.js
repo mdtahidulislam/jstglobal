0db76bb2085c4d87ac47071e466b58b58ef695a4
mdtahidulislam <tahidul.developer@gmail.com>
2023-01-31 11:51:22 +0600

    added updated content

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -167,13 +167,5 @@
         // interSectObserver('.js-img-overlay');
 
         revealSpltTextAnim('.mobile-menu li a');
-
-        // fix 100vh for ios
-        const appHeight = () => {
-            const doc = document.documentElement
-            doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-        }
-        window.addEventListener('resize', appHeight)
-        appHeight();
     });
 }(jQuery));
